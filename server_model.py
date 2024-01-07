56bf35f6626603f6fa2a88869b721c04dda3459e
dangtrantrung <trung280584@gmail.com>
2024-01-07 15:35:21 +0700

    server_model and index Page

---
+++ server_model.py
@@ -0,0 +1,20 @@
+
+from flask import Flask, render_template
+from flask_cors import CORS, cross_origin
+
+# Khởi tạo Flask
+app = Flask(__name__)
+
+# Apply Flask - CORS
+CORS(app)
+app.config['CORS_HEADERS'] ='Content-Type'
+
+
+# Hàm xử lý request
+@app.route("/", methods=['GET'])
+def home_page():
+    return render_template('index.html')
+
+
+if __name__ == '__main__':
+    app.run(ssl_context='adhoc',host='0.0.0.0', port=5000)
